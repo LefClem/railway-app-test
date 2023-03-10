e127ccd164ae97770c9f7a9b577d4298ad33d43a
LefClem <lefranc.clment7@gmail.com>
2023-03-10 14:48:39 +0100

    Update App.js

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ app.get('/home', async (req, res, next) => {
 })
 
 app.get('/depense', (req, res, next) => {
-    let sql = 'SELECT * FROM ' + db.config.database + ".task";
+    let sql = 'SELECT * FROM ' + db.config.database + ".Spending";
     db.query(sql, (err, results) => {
         try {
             if(err) throw err;
             res.send(results)
         } catch (error) {
             res.send(error)
-            console.error(error
-                );
+            console.error(error);
         }
     })
 })
